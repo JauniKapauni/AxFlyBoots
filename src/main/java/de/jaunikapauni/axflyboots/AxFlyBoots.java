3118f49a4ee647b22bccafe0649e1388be4f684b
JauniKapauni <riches-goods.54@icloud.com>
2026-05-20 15:05:31 +0200

    feat: add PlayerArmorChangeListener

--- src/main/java/de/jaunikapauni/axflyboots/AxFlyBoots.java
+++ src/main/java/de/jaunikapauni/axflyboots/AxFlyBoots.java
@@ -1,12 +1,13 @@
 package de.jaunikapauni.axflyboots;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import de.jaunikapauni.axflyboots.listener.PlayerArmorChangeListener;import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxFlyBoots extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getServer().getPluginManager().registerEvents(new PlayerArmorChangeListener(), this);
     }
 
     @Override
---
+++ src/main/java/de/jaunikapauni/axflyboots/listener/PlayerArmorChangeListener.java
@@ -0,0 +1,29 @@
+package de.jaunikapauni.axflyboots.listener;
+
+import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+
+public class PlayerArmorChangeListener implements Listener {
+
+    @EventHandler
+    public void onArmorChange(PlayerArmorChangeEvent e){
+        Player p = e.getPlayer();
+        ItemStack newBoots = e.getNewItem();
+        ItemStack oldBoots = e.getOldItem();
+        if(newBoots.getType().name().contains("BOOTS")){
+            if(!p.getAllowFlight()){
+                p.setAllowFlight(true);
+                p.sendMessage("Fly activated!");
+            }
+        } else {
+            if(oldBoots.getType().name().contains("BOOTS")){
+                p.setAllowFlight(false);
+                p.setFlying(false);
+                p.sendMessage("Fly deactived!");
+            }
+        }
+    }
+}
