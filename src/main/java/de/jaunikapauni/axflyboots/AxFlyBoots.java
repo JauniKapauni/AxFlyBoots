f9356d6d37550eca8b6a64205486c3b24af85f4d
JauniKapauni <riches-goods.54@icloud.com>
2026-05-20 15:02:09 +0200

    init: initialize project

---
+++ src/main/java/de/jaunikapauni/axflyboots/AxFlyBoots.java
@@ -0,0 +1,16 @@
+package de.jaunikapauni.axflyboots;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxFlyBoots extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
